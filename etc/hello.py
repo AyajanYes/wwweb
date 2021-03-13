ca9fc4e56ce5d6203fc5fa7cc17e01d1cb91bc85
AyajanYes <71542329+AyajanYes@users.noreply.github.com>
2021-03-13 13:05:08 +0600

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,11 +1,3 @@
-  CONFIG = {
-    'mode': 'wsgi',
-    'working_dir': '/home/box/web/ask',
-    'python': '/usr/bin/python',
-    'args': (
-        '--bind=0.0.0.0:8000',
-        '--workers=16',
-        '--timeout=60',
-        'ask.wsgi:application',
-    ),
-}
+pythonpath = '/home/box/web/'  # путь до приложения
+bind = "0.0.0.0:8080"
+workers = 4
