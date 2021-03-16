3fcf42d07b135e7db486ea71c0914f9ed17b6aca
AyajanYes <71542329+AyajanYes@users.noreply.github.com>
2021-03-16 21:18:08 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf 
 sudo /etc/init.d/nginx restart
-gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app & ask.wsgi:application & curl -vv 'http://127.0.0.1:8080/login/'
+gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app & ask.wsgi:application & curl -vv 'http://127.0.0.1:8000/login/'
 sudo /etc/init.d/gunicorn restart
