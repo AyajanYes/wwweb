0a7e69ddb440ccbf123f75d2405e97bbda759669
AyajanYes <71542329+AyajanYes@users.noreply.github.com>
2021-03-31 16:27:11 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,3 @@
-sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/nginx.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:wsgi_get_query_pars &
-sudo gunicorn -c /home/box/web/etc/ask.py ask.wsgi &
