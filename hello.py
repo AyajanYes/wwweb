d88d8aef439828c2c59fbbd6d0d536cf254b9c05
AyajanYes <71542329+AyajanYes@users.noreply.github.com>
2021-04-13 14:29:19 +0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,18 @@
-def wsgi_get_query_pars(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [bytes('\n'.join(environ['QUERY_STRING'].split('&')), 'utf8')]
+def app(environ, start_response):
+
+	
+	raw_uri = str(environ.get('RAW_URI'))
+	
+	raw_uri = raw_uri[2:]
+
+	params = raw_uri.split('&')
+	
+	data = ''
+	for param in params:
+		data += param + '\r\n'
+	
+	start_response("200 OK", [
+	  ("Content-Type", "text/plain"),
+	  ("Content-Length", str(len(data)))
+	])
+	return iter([data])
