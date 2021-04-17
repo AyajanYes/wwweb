3f5b9e7ee33fa2f4b404a46c088fdd3899ef89fb
AyajanYes <71542329+AyajanYes@users.noreply.github.com>
2021-04-17 11:06:39 +0600

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
          'ENGINE': 'django.db.backends.mysql',
          'NAME': 'qa',
          'USER': 'root',
-         'PASSWORD': '',
+         'PASSWORD': 'passwrd123',
          'HOST': 'localhost',
          'PORT': '',
      }
